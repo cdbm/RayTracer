4544a107b41c063edb7db190853305dd32523cb8
cdbm <cdbm@cin.ufpe.br>
2019-05-11 21:32:23 -0300

    arrumando main

--- wknd/main.cpp
+++ wknd/main.cpp
@@ -6,16 +6,6 @@
 #include "float.h"
 using namespace std;
 
-/*bool hit_sphere(const vec3& center, float radius, const ray& r){
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc,oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
-    return  (discriminant>0);
-}*/
-
-
 vec3 color (const ray& r, hitable *world){
     hit_record rec;
     if(world->hit(r, 0.0, MAXFLOAT, rec)){
